1f0e2bfb8ebd683e53fc99d5c880e0b6501db980
andersonaguiar <54728366+andersonaguia@users.noreply.github.com>
2022-08-29 12:44:05 -0300

    [M1S09] Ex 6 - Criar esqueleto da nova página de produtos selecionados

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Produtos } from "../pages/produtos/Produtos";
+import { ProdutosSelecionados } from "../pages/produtosSelecionados/ProdutosSelecionados";
 
 export const Router = () => {
   return(
     <Routes>
       <Route path="/" element={<Produtos />} />
+      <Route path="/selecionados" element={<ProdutosSelecionados />} />
     </Routes>
   )    
 };
