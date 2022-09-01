14187b2855019001a51194b7cd7f3621759fdaa6
andersonaguiar <54728366+andersonaguia@users.noreply.github.com>
2022-09-01 09:07:32 -0300

    [M1S09] Ex 9 - Adicionar total dos produtos selecionados

--- src/pages/produtosSelecionados/ProdutosSelecionados.jsx
+++ src/pages/produtosSelecionados/ProdutosSelecionados.jsx
@@ -9,11 +9,18 @@ export const ProdutosSelecionados = () => {
         return(
             selecionados.map((id) => produtos.find((produto) => produto.id === id))
         )        
-    }  
-    
+    }   
+
+    const total = () => {
+        return(
+            listaSelecionados().reduce((soma, item) => soma + item.valor, 0)
+        )
+    }
+
     return(
         <>
             <h1>Produtos Selecionados</h1>
+            <p>Total: R$ {total().toFixed(2).replace(".", ",")}</p>
             <ListaCards produtos={listaSelecionados()}/>
         </>       
     )
